a07c3a8cd9978464bcac79c80cef44f2923370f4
Aviv274 <aviv275@student.ie.edu>
2025-03-03 21:11:20 +0100

    Add files via upload
    inheritance

--- ml/reg_model.py
+++ ml/reg_model.py
@@ -1,5 +1,4 @@
 import joblib
-import logging
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression, ElasticNet
@@ -10,57 +9,38 @@ import os
 import json
 from imblearn.over_sampling import SMOTE
 from sklearn.ensemble import RandomForestClassifier
+from StockPricePredictor import StockPricePredictor, load_config, configure_global_logging
 
 
-class StockPricePredictor:
+class LogisticRegrModel(StockPricePredictor):
     def __init__(self, stock_name, config, logger):
+        super().__init__(stock_name,config,logger)
         """Initialize the model, scalers, and preprocess data."""
-        self.stock_name = stock_name
-        self.file_path = f"{config['data_directory']}/{stock_name}_merged_data.csv"
-        self.logger = logger
-
-        # Directories for saving models and scalers
-        self.models_directory = config["models_directory"]
-        self.scalers_directory = config["scalers_directory"]
-        self.features_directory = config["features_directory"]
-
-        # Model parameters
         self.scaler = StandardScaler()
         self.elastic_net = ElasticNet(
             alpha=config["alpha"], 
             l1_ratio=config["l1_ratio"], 
             random_state=config["random_state"]
         )
+        self.cv = config["cv"]
+        self.scoring = config["scoring"]
         self.top_features = config["top_features"]
-        self.test_size = config["test_size"]
         self.random_state = config["random_state"]
-
         self.columns_to_drop = list(config["columns_to_drop"])
-
-        # Load dataset
-        self.df = pd.read_csv(self.file_path)
-        self.X, self.y = self.preprocess_data(self.df)
-
-        # Perform train-test split, SMOTE, and scaling once
+        self.X, self.y = self.preprocess_data()
         self.preprocess_data_for_training()
-
         self.best_params = None
-        self.model = None
-
     
-    def preprocess_data(self, df):
+    def preprocess_data(self):
         """Preprocess dataset: Feature selection and scaling."""
-        df['Date'] = pd.to_datetime(df['Date'])
-        df = df.sort_values(by=['Date'])
-        df['Target'] = (df['Close'] > df['Close'].shift(-1)).astype(int)
-        df = df[:-1]  # Drop last row with NaN target
-
-        # Drop specified columns
-        df = df.drop(columns=self.columns_to_drop, errors='ignore')
+        super().preprocess_data()
+        self.df['Target'] = (self.df['Close'] > self.df['Close'].shift(-1)).astype(int)
+        self.df = self.df[:-1]
+        self.df = self.df.drop(columns=self.columns_to_drop, errors='ignore')
         
-        feature_candidates = df.select_dtypes(include=['number']).columns.drop(['Target'])
-        X_all = df[feature_candidates]
-        y_all = df['Target']
+        feature_candidates = self.df.select_dtypes(include=['number']).columns.drop(['Target'])
+        X_all = self.df[feature_candidates]
+        y_all = self.df['Target']
 
         # Feature selection using ElasticNet
         X_scaled = MinMaxScaler().fit_transform(X_all)  # Use MinMaxScaler for ElasticNet
@@ -76,7 +56,7 @@ class StockPricePredictor:
             json.dump(self.selected_features, f)
 
         self.logger.info(f"Selected features for {self.stock_name}: {self.selected_features}")
-        return df[self.selected_features], df['Target']
+        return self.df[self.selected_features], self.df['Target']
 
 
     def preprocess_data_for_training(self):
@@ -84,7 +64,6 @@ class StockPricePredictor:
         X_train, X_test, y_train, y_test = train_test_split(
             self.X, self.y, test_size=self.test_size, random_state=self.random_state, shuffle=True
         )
-
         # Apply SMOTE once
         smote = SMOTE(sampling_strategy='auto', random_state=42)
         self.X_train_resampled, self.y_train_resampled = smote.fit_resample(X_train, y_train)
@@ -104,8 +83,8 @@ class StockPricePredictor:
         grid_search = GridSearchCV(
             RandomForestClassifier(random_state=self.random_state),
             param_grid,
-            cv=5,
-            scoring='f1',
+            cv=self.cv,
+            scoring=self.scoring,
             n_jobs=-1
         )
 
@@ -151,69 +130,6 @@ class StockPricePredictor:
 
         return self.model.predict(X_new_scaled)
 
-    
-    def save_model(self):
-            """Save the trained model, scaler, and selected features with stock name."""
-            # Ensure directories exist
-            os.makedirs(self.features_directory, exist_ok=True)
-            os.makedirs(self.models_directory, exist_ok=True)
-            os.makedirs(self.scalers_directory, exist_ok=True)
-            
-            feature_file = f"{self.features_directory}/{self.stock_name}_selected_features.json"
-            model_path = f"{self.models_directory}/{self.stock_name}_logistic_regression_model.pkl"
-            scaler_path = f"{self.scalers_directory}/{self.stock_name}_logistic_scaler.pkl"
-            
-            joblib.dump(self.model, model_path)
-            joblib.dump(self.scaler, scaler_path)
-            
-            with open(feature_file, "w") as f:
-                json.dump(self.selected_features, f)
-            
-            self.logger.info(f"Model saved as {model_path}, Scaler saved as {scaler_path}, Features saved as {feature_file}.")
-
-    
-    def load_model(self):
-        """Load the trained model, scaler, and selected features."""
-        model_path = f"models/{self.stock_name}_logistic_regression_model.pkl"
-        scaler_path = f"scalers/{self.stock_name}_scaler.pkl"
-        feature_file = f"models/{self.stock_name}_selected_features.json"
-
-        self.model = joblib.load(model_path)
-        self.scaler = joblib.load(scaler_path)
-
-        # Load selected features
-        with open(feature_file, "r") as f:
-            self.selected_features = json.load(f)
-
-        self.logger.info(f"Model, scaler, and selected features for {self.stock_name} loaded successfully.")
-
-
-
-def load_config(config_path):
-    """Loads the configuration file."""
-    if not os.path.exists(config_path):
-        print("Config file not found! Using default settings.")
-        return {}
-
-    try:
-        with open(config_path, 'r') as file:
-            return json.load(file) or {}
-    except json.JSONDecodeError:
-        print("Failed to load config.json. Using default settings.")
-        return {}
-
-    
-def configure_global_logging(config):
-    """Sets up logging for the main process using config."""
-    logging.basicConfig(
-        filename=config["log_file"],
-        filemode="a",
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO
-    )
-    return logging.getLogger(__name__)
-
 
 if __name__ == "__main__":
     
@@ -221,7 +137,7 @@ if __name__ == "__main__":
     logger = configure_global_logging(config)
 
     for ticker in config["tickers"]:
-        predictor = StockPricePredictor(ticker, config, logger)
+        predictor = LogisticRegrModel(ticker, config, logger)
         
         predictor.tune_hyperparameters(config)
         predictor.train()
